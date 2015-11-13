1685c921e5316276b957a36241b4d915338c81ee
Dheera Venkatraman <dheera@dheera.net>
2015-11-13 16:46:52 +0000

    scrolling

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -76,10 +76,10 @@ $(function() {
 
 // todo: build an actual Polymer component
 function page(index) {
-  _('.viewpager-fragments').style.WebkitTransform = 'translateX(' + (-index*100.0/3) + '%)';
-  _('.viewpager-fragments').style.MozTransform = 'translateX(' + (-index*100.0/3) + '%)';
-  _('.viewpager-fragments').style.msTransform = 'translateX(' + (-index*100.0/3) + '%)';
-  _('.viewpager-fragments').style.OTransform = 'translateX(' + (-index*100.0/3) + '%)';
+  _('.viewpager-fragments').style.WebkitTransform = 'translateX(' + (-index*100.0) + '%)';
+  _('.viewpager-fragments').style.MozTransform = 'translateX(' + (-index*100.0) + '%)';
+  _('.viewpager-fragments').style.msTransform = 'translateX(' + (-index*100.0) + '%)';
+  _('.viewpager-fragments').style.OTransform = 'translateX(' + (-index*100.0) + '%)';
   if(index === 0) {
     _('#nav-back').style.display='none';
     _('#nav-menu').style.display='';
