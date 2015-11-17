b991a706285c4447d460285287cadea1cb7f4f6a
Dheera Venkatraman <dheera@dheera.net>
2015-11-17 17:44:48 +0000

    search

--- server/views/search.py
+++ server/views/search.py
@@ -11,7 +11,11 @@ def show():
   # because Google is deprecating this API and not providing a
   # usable replacement
 
-  query = request.args.get('q','')
+  query = request.args.get('q','').strip()
+
+  if query == '':
+    return '[]'
+
   url = 'https://ajax.googleapis.com/ajax/services/search/web'
   params = {
     'v': '1.0',
@@ -25,7 +29,11 @@ def show():
   results = response.get('responseData',{}).get('results',[])
   results_out = []
   for result in results:
-    if 'Category:' not in result['titleNoFormatting'] and 'List of' not in result['titleNoFormatting'] and 'Talk:' not in result['titleNoFormatting']:
+    if 'Category:' not in result['titleNoFormatting'] and \
+       'List of' not in result['titleNoFormatting'] and \
+       'Talk:' not in result['titleNoFormatting'] and \
+       'User:' not in result['titleNoFormatting'] and \
+       'IMSLP:' not in result['titleNoFormatting']:
       results_out.append({
         'url': result['unescapedUrl'],
         'title': result['titleNoFormatting'].split(' - ',1)[0],
